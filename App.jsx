fc04ded3e749d3b61238b6fd98333e99aa5b962e
Muhuned <catalonia10@gmail.com>
2025-06-25 10:04:17 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import beeLogo from './assets/bee-logo-yellow.png';
+
+export default function App() {
+  return (
+    <main>
+      <h1>FXBee App is Live</h1>
+      <img src={beeLogo} alt="Bee Logo" />
+    </main>
+  );
+}
