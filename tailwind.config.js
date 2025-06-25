9a99e83d26973d78d61f508a9e3a894e45dab585
Muhuned <catalonia10@gmail.com>
2025-06-25 11:35:47 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports = {
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          50: "#fffce0",
+          100: "#fff6b8",
+          200: "#ffee70",
+          400: "#ffd02c",
+          500: "#ffc107",
+          600: "#ffb000"
+        }
+      },
+    },
   },
   plugins: [],
 }
